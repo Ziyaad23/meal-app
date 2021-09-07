d76c1779fe4e9925f5701efc2295847eb3e90dc2
Ziyaad <rhyman.ziyaad@gmail.com>
2021-09-07 22:34:07 +0400

    Initial commit

---
+++ app/src/main/java/com/ziyaad/meal_app/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.ziyaad.meal_app
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.ziyaad.meal_app.ui.theme.MealappTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MealappTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    MealappTheme {
+        Greeting("Android")
+    }
+}
