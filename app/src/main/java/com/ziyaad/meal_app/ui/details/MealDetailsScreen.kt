70b99a67d02366871992a54a186692b4bd78340e
Ziyaad <rhyman.ziyaad@gmail.com>
2021-09-11 23:41:33 +0400

    [+][ADD] Add animation profile image

--- app/src/main/java/com/ziyaad/meal_app/ui/details/MealDetailsScreen.kt
+++ app/src/main/java/com/ziyaad/meal_app/ui/details/MealDetailsScreen.kt
@@ -1,17 +1,23 @@
 package com.ziyaad.meal_app.ui.details
 
+import androidx.compose.animation.animateColor
+import androidx.compose.animation.core.animateDp
 import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.updateTransition
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Button
 import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberImagePainter
@@ -21,12 +27,22 @@ import com.ziyaad.meal_app.ui.model.response.MealResponse
 @Composable
 fun MealDetailsScreen(meal: MealResponse?) {
 
-    var isExpanded by remember() { mutableStateOf(false) }
-    val imageSizeDp: Dp by animateDpAsState(targetValue = if (isExpanded) 200.dp else 100.dp)
+    var profilePictureSate by remember() { mutableStateOf(MealProfilePictureSate.Normal) }
+    val transition = updateTransition(targetState = profilePictureSate, label = "")
+    val imageSizeDp by transition.animateDp(targetValueByState = { it.size }, label = "")
+    val color by transition.animateColor(targetValueByState = { it.color }, label = "")
+    val widthSize by transition.animateDp(targetValueByState = { it.borderWidth }, label = "")
+
+
 
     Column() {
         Row() {
-            Card() {
+            Card(
+                modifier = Modifier.padding(16.dp), shape = CircleShape, border = BorderStroke(
+                    width = widthSize,
+                    color = color
+                )
+            ) {
                 Image(
                     painter = rememberImagePainter(data = meal?.imageUrl, builder = {
                         transformations(CircleCropTransformation())
@@ -45,8 +61,18 @@ fun MealDetailsScreen(meal: MealResponse?) {
             )
         }
         Button(modifier = Modifier.padding(16.dp),
-            onClick = { isExpanded = !isExpanded }) {
+            onClick = {
+                profilePictureSate = if (profilePictureSate == MealProfilePictureSate.Normal)
+                    MealProfilePictureSate.Expanded
+                else
+                    MealProfilePictureSate.Normal
+            }) {
             Text("Change state of meal profile picture")
         }
     }
+}
+
+enum class MealProfilePictureSate(val color: Color, val size: Dp, val borderWidth: Dp) {
+    Normal(Color.Magenta, 120.dp, 8.dp),
+    Expanded(Color.Green, 200.dp, 24.dp)
 }
